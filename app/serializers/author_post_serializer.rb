a152f7b39fa42512621941c01b7bc307336829f7
Tyler <toygrrr@gmail.com>
2022-09-23 16:32:19 -0700

    done done

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,7 +1,6 @@
 class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content
   has_many :tags
-
   def short_content
     #implied self self.object.content
     "#{object.content[0..39]}..."
